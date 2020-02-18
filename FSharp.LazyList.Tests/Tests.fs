0ddbeeb164045ea8def1a3345070b03712f82f95
Rex Ng <rexcfnghk@gmail.com>
2020-02-18 22:17:52 +0800

    Add tests

--- FSharp.LazyList.Tests/Tests.fs
+++ FSharp.LazyList.Tests/Tests.fs
@@ -1,13 +1,24 @@
-module Tests
+module FSharp.LazyList.Tests
 
+open FSharp.LazyList
 open Hedgehog
 open Swensen.Unquote
 open Xunit
 
 [<Fact>]
-let ``toList round trip to original list`` () =
+let ``toList round trips to original list`` () =
     Property.check <| property {
         let! list = Range.linear 1 1000 |> Gen.list <| Gen.alphaNum
         let sut = List.fold (flip LazyList.cons) LazyList.empty list
         LazyList.toList sut =! list
+    }
+    
+[<Fact>]
+let ``toSeq round trips to original seq`` () =
+    Property.check <| property {
+        let! seq = Range.linear 1 1000 |> Gen.list <| Gen.alphaNum
+        let sut = Seq.fold (flip LazyList.cons) LazyList.empty seq
+        LazyList.toSeq sut
+        |> Seq.toList
+        |> (=!) seq
     }
--- FSharp.LazyList/LazyList.fs
+++ FSharp.LazyList/LazyList.fs
@@ -1,54 +1,70 @@
-[<RequireQualifiedAccess>]
-module LazyList
+namespace FSharp.LazyList
 
-open Lazy
-open Utilities
+open System
+open System.Text
 
 [<StructuredFormatDisplay("{AsString}")>]
 type LazyList<'a> =
     | Empty
     | LazyList of head: Lazy<'a> * tail: LazyList<'a>
+    override this.ToString () =
+        let rec folder (acc: StringBuilder) = function
+            | Empty -> acc.ToString ()
+            | LazyList (Lazy x, xs) -> folder (sprintf "%A, " x |> acc.Append) xs
+        let content = folder (StringBuilder ()) this
+        "[" + content + "]"
+    member this.AsString = this.ToString () 
 
-let empty = Empty
+[<RequireQualifiedAccess>]
+module LazyList =
 
-let mkLazyList x xs = LazyList (x, xs)
+    open Lazy
+    open Utilities
 
-let singleton x = x |> Lazy.returnLazy |> flip mkLazyList empty
+    let empty = Empty
 
-let cons x xs = LazyList (Lazy.returnLazy x, xs)
+    let mkLazyList x xs = LazyList (x, xs)
 
-let concat xs ys =
-    let rec concatImpl acc xs ys =
-        match (xs, ys) with
-        | Empty, Empty -> acc
-        | Empty, ys -> concatImpl acc ys empty 
-        | LazyList (Lazy x, xs), ys -> concatImpl (cons x acc) xs ys
-    concatImpl empty xs ys
-    
-let ( ++ ) = concat
+    let singleton x = x |> Lazy.returnLazy |> flip mkLazyList empty
 
-let rec map f = function
-    | Empty -> Empty
-    | LazyList (x, xs) -> mkLazyList (f x) (map f xs)
-    
-let ( <!> ) = map
+    let cons x xs = LazyList (Lazy.returnLazy x, xs)
 
-let rec apply fLazyList xLazyList =
-    match (fLazyList, xLazyList) with
-    | LazyList (Lazy f, fs), LazyList (Lazy x, xs) ->
-        mkLazyList (lazy (f x)) (apply fs xs)
-    | _ -> Empty
-    
-let rec foldr f acc = function
-    | Empty -> acc
-    | LazyList (Lazy x, xs) -> f x (foldr f acc xs)
-    
-let rec foldl f acc = function
-    | Empty -> acc
-    | LazyList (Lazy x, xs) -> foldl f (f acc x) xs
-    
-let rec bind f = function
-    | Empty -> Empty
-    | LazyList (Lazy x, xs) -> f x ++ bind f xs
+    let concat xs ys =
+        let rec concatImpl acc xs ys =
+            match (xs, ys) with
+            | Empty, Empty -> acc
+            | Empty, ys -> concatImpl acc ys empty 
+            | LazyList (Lazy x, xs), ys -> concatImpl (cons x acc) xs ys
+        concatImpl empty xs ys
+        
+    let ( ++ ) = concat
 
-let toList xs = foldl (fun acc x -> x :: acc) [] xs
+    let rec map f = function
+        | Empty -> Empty
+        | LazyList (x, xs) -> mkLazyList (f x) (map f xs)
+        
+    let ( <!> ) = map
+
+    let rec apply fLazyList xLazyList =
+        match (fLazyList, xLazyList) with
+        | LazyList (Lazy f, fs), LazyList (Lazy x, xs) ->
+            mkLazyList (lazy (f x)) (apply fs xs)
+        | _ -> Empty
+        
+    let rec foldr f acc = function
+        | Empty -> acc
+        | LazyList (Lazy x, xs) -> f x (foldr f acc xs)
+        
+    let rec foldl f acc = function
+        | Empty -> acc
+        | LazyList (Lazy x, xs) -> foldl f (f acc x) xs
+        
+    let rec bind f = function
+        | Empty -> Empty
+        | LazyList (Lazy x, xs) -> f x ++ bind f xs
+
+    let toList xs = foldl (fun acc x -> x :: acc) [] xs
+
+    let toSeq xs =
+        let cons xs = flip Seq.append xs << Seq.singleton 
+        foldl cons Seq.empty xs
